e1a83dce1b73d8bf25680a3363da8d9d9f9fba85
tony <anthony.ervin10@gmail.com>
2024-07-29 18:43:39 -0700

    Update Robots file

--- app/robots.js
+++ app/robots.js
@@ -5,6 +5,6 @@ export default function robots() {
 			allow: '/',
 			disallow: '/private/',
 		},
-		sitemap: 'http://localhost:3000/sitemap.xml',
+		sitemap: 'https://www.tiktokanalytics.tech/sitemap.xml',
 	}
 }
